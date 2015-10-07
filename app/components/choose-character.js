b21b5b252f495b68cb1ecc775eff0268e29b6a47
Shannon Mulloy & Michael Summers <student@epicodus.com>
2015-10-07 13:49:30 -0700

    create, shuffle and deal cards

--- app/components/choose-character.js
+++ app/components/choose-character.js
@@ -3,19 +3,56 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     newGame(character) {
-      var params = {
-        character: character.get('name'),
-        guesses: [],
-        rooms: this.get('model.rooms'),
-        suspects: this.get('model.suspects'),
-        weapons: this.get('model.weapons'),
-        answer: {
-          who: this.get('model.suspects').objectAt(Math.floor(Math.random()*6)).get('name'),
-          where: this.get('model.rooms').objectAt(Math.floor(Math.random()*6)).get('name'),
-          how: this.get('model.weapons').objectAt(Math.floor(Math.random()*6)).get('name')
+      var player = {name: character.get('name'), cards: [], guesses: []},
+          opponents = [],
+          cardArray = [],
+          suspects = this.get('model.suspects'),
+          rooms = this.get('model.rooms'),
+          weapons = this.get('model.weapons');
+      for(var i = 0; i < suspects.get('length'); i++) {
+        cardArray.push(suspects.objectAt(i).get('name'));
+        if(suspects.objectAt(i).get('name') !== character.get('name')) {
+          opponents.push({name: suspects.objectAt(i).get('name'), cards: [], guesses: []});
         }
+      }
+      for(var i = 0; i < rooms.get('length'); i++) {
+        cardArray.push(rooms.objectAt(i).get('name'));
+      }
+      for(var i = 0; i < weapons.get('length'); i++) {
+        cardArray.push(weapons.objectAt(i).get('name'));
+      }
+      var who = (cardArray.splice(Math.floor(Math.random()*6), 1)).toString(),
+          where = (cardArray.splice(Math.floor(Math.random() * 9 + 5), 1)).toString(),
+          how = (cardArray.splice(Math.floor(Math.random() * 6 + 13), 1)).toString(),
+          shuffle = function(arr) {
+            var copy = [], n = arr.length, i;
+            while (n) {
+              i = Math.floor(Math.random() * n--);
+              copy.push(arr.splice(i, 1)[0]);
+            }
+            return copy;
+          };
+      cardArray = shuffle(cardArray);
+      player.cards = cardArray.splice(0, 3);
+      for(var i = 0; i < opponents.length; i++) {
+        opponents[i].cards = cardArray.splice(0,3);
+      }
+      console.log(cardArray.length);
+      var params = {
+        character: player,
+        guesses: [],
+        rooms: rooms,
+        suspects: suspects,
+        weapons: weapons,
+        answer: {
+          who: who,
+          where: where,
+          how: how,
+        },
+        cards: cardArray,
+        opponents: opponents,
       };
-      console.log(params.answer.who);
+      console.log(params.answer.who + " in the " + params.answer.where + " with the " + params.answer.how);
       this.sendAction('newGame', params);
     }
   }
